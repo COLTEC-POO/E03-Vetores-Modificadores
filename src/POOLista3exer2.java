48eb76dfcc0e47735afab639b62f9d8c5f862445
13RodriguesBruna <107412241+13RodriguesBruna@users.noreply.github.com>
2022-10-17 14:56:34 -0300

    Update POOLista3exer2.java

--- src/POOLista3exer2.java
+++ src/POOLista3exer2.java
@@ -1,149 +1,38 @@
-public class conta{
-  int numero;
-  String titular;
-  double saldo;
-  double limite;
-  String operacao;
-  
-  
+//class main
+public class Main {
+    public static void main(String[] args) {
+
+        ContaExtrato c1 = new ContaExtrato();
+
+        c1.depositar1 = 500;
+        c1.sacar1 = 200;
+        c1.sacar2 = 3000;
+        c1.depositar2 = 150;
+        c1.sacar3 = 50;
+
+        c1.imprimirExtrato();
+    }
 }
 
+//class extrato
+public class ContaExtrato {
+    int depositar1;
+    int depositar2;
+
+    int sacar1;
+    int sacar2;
+    int sacar3;
 
 
-//classe main
-public class main {
-  public static void main(string[]args){
-    conta minhaconta= new conta();
-      
-    minhaconta.titular='Maria';
-    minhaconta.numero='0001-1';
-    minhaconta.saldo='10.000,00';
-    minhaconta.limite='5.0000,00';
-	minhaconta.operacao='1000';
-    
-    System.out.println(minhaconta.titular);
-    System.out.println(minhaconta.numero);
-    System.out.println(minhaconta.saldo);
-    System.out.println(minhaconta.limite);
-	System.out.println(minhaconta.operacao);
+    public void imprimirExtrato(){
+        System.out.println(depositar1);
+        System.out.println(depositar2);
+        System.out.println(sacar1);
+        System.out.println(sacar2);
+        System.out.println(sacar3);
 
-
-   }
-}
-
-public void imprimir{
-
-  minhaconta.nomeconta='José';
-  minhaconta.numeroconta='02020-91';
-  minhaconta.saldoatual='10.000';
-  minhaconta.limitecartaocredito='30.000';
-  
-
-  System.out.println(minhaconta.nomeconta);
-  Systen.out.println(minhaconta.numeroconta);
-  System.out.println(minhaconta.saldoatual);
-  Systen.out.println(minhaconta.limitecartaocredito);
-  
-  //classe cliente;
-
-public class cliente{
-  String nome;
-  String CPF;
-  String Endereco;
-  int idade;
-  char sexo;
-}
-
-//clase main com impressões
-
-public static void main(String[]args){
-
-  cliente meucliente = new cliente();
-  
-  meucliente.nome='Jose';
-  meucliente.CPF='01010101011';
-  meucliente.endereco='rua A, numero C, bairro D, Cidade E';
-  meucliente.idade='25';
-  meucliente.sexo='F';
-
-System.out.println(meucliente.nome);
-System.out.println(meucliente.CPF);
-System.out.println(meucliente.endereco);
-Sytem.out.println(meucliente.idade);
-System.out.println(meucliente.sexo);
-
-}
-}	
-  
-//lista 3 exercicio 2
-public static void main(String[]args){
-
-public class Conta{
-     String NomeTitular;
-     int NumeroConta;
-     float SaldoConta;
-  
-  public static void main(String[]args){
-  
-  Conta extrato = new Conta();
-  extrato.NomeTitular(Maria Aparecida);
-  extrato.NumeroConta(000598-47);
-  extrato.SaldoConta(50000.00);
-  
-  
-
-    public void ImprimirExtrato(){
-            System.out.println("Nome do titular: " + NomeTitular);
-            System.out.println("Numero da Conta: " + NumeroConta);
-            System.out.println("Saldo atual da conta: " + SaldoConta);
-       }
-
-}
-
-
-
-
-
-
-**
- * Operacao.java
- *
- * @author João Eduardo Montandon
- */
-
-import java.util.Date;
-
-/**
- * Classe responsável por registrar operações de saque e depósitos realizados em contas correntes.
- */
-public class Operacao {
-
-    /* Data de realização da operação */
-    public Date data;
-
-    /* Tipo da operação */
-    public char tipo;
-
-    /* Valor da operação */
-    public double valor;
-
-    /**
-     * Construtor. Inicializa uma nova instância da classe Operacao onde a data da operação é exatamente a data
-     * da criação da classe.
-     *
-     * Exemplos de uso:
-     *
-     * > Operacao op1 = new Operacao('d', 2500); // Operação de depósito de 2500 reais
-     * > Operacao op2 = new Operacao('s', 1000); // Operação de saque de 1000 reais
-     *
-     * @param tipo Tipo da operação, podendo ser 'd' ou 's'
-     * @param valor Valor da operação
-     */
-    public Operacao(char tipo, double valor) {
-        this.tipo = tipo;
-        this.valor = valor;
-        data = new Date();
-		
     }
 
 }
+
+

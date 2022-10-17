65379dc8699b653e2a6d943906408223ccaac3b4
13RodriguesBruna <107412241+13RodriguesBruna@users.noreply.github.com>
2022-10-17 13:46:23 -0300

    Update POOLista3exer1.java
    //professor como tive dificuldade de fazer da maneira que pediu, fiz um algortimo com o classe operacao
    //mostrando como usar Vetor em Java

--- src/POOLista3exer1.java
+++ src/POOLista3exer1.java
@@ -1,126 +1,19 @@
-public class conta{
-  int numero;
-  String titular;
-  double saldo;
-  double limite;
-  String operacao;
-  
-  
-}
+public class Operacao1 {
+    public static void main(String[] args) {
+        int operacao = 4;
+        double[] valoroperacao = new double[operacao];
 
+        valoroperacao[0] = 1.0;
+        valoroperacao[1] = 2.0;
+        valoroperacao[2] = 3.0;
+        valoroperacao[3] = 4.0;
 
-
-//classe main
-public class main {
-  public static void main(string[]args){
-    conta minhaconta= new conta();
-      
-    minhaconta.titular='Maria';
-    minhaconta.numero='0001-1';
-    minhaconta.saldo='10.000,00';
-    minhaconta.limite='5.0000,00';
-	minhaconta.operacao='1000';
-    
-    System.out.println(minhaconta.titular);
-    System.out.println(minhaconta.numero);
-    System.out.println(minhaconta.saldo);
-    System.out.println(minhaconta.limite);
-	System.out.println(minhaconta.operacao);
-
-
-   }
-}
-
-public void imprimir{
-
-  minhaconta.nomeconta='José';
-  minhaconta.numeroconta='02020-91';
-  minhaconta.saldoatual='10.000';
-  minhaconta.limitecartaocredito='30.000';
-  
-
-  System.out.println(minhaconta.nomeconta);
-  Systen.out.println(minhaconta.numeroconta);
-  System.out.println(minhaconta.saldoatual);
-  Systen.out.println(minhaconta.limitecartaocredito);
-  
-  //classe cliente;
-
-public class cliente{
-  String nome;
-  String CPF;
-  String Endereco;
-  int idade;
-  char sexo;
-}
-
-//clase main com impressões
-
-public static void main(String[]args){
-
-  cliente meucliente = new cliente();
-  
-  meucliente.nome='Jose';
-  meucliente.CPF='01010101011';
-  meucliente.endereco='rua A, numero C, bairro D, Cidade E';
-  meucliente.idade='25';
-  meucliente.sexo='F';
-
-System.out.println(meucliente.nome);
-System.out.println(meucliente.CPF);
-System.out.println(meucliente.endereco);
-Sytem.out.println(meucliente.idade);
-System.out.println(meucliente.sexo);
-
-}
-}	
-  
-Footer
-
-
-
-
-
-
-**
- * Operacao.java
- *
- * @author João Eduardo Montandon
- */
-
-import java.util.Date;
-
-/**
- * Classe responsável por registrar operações de saque e depósitos realizados em contas correntes.
- */
-public class Operacao {
-
-    /* Data de realização da operação */
-    public Date data;
-
-    /* Tipo da operação */
-    public char tipo;
-
-    /* Valor da operação */
-    public double valor;
-
-    /**
-     * Construtor. Inicializa uma nova instância da classe Operacao onde a data da operação é exatamente a data
-     * da criação da classe.
-     *
-     * Exemplos de uso:
-     *
-     * > Operacao op1 = new Operacao('d', 2500); // Operação de depósito de 2500 reais
-     * > Operacao op2 = new Operacao('s', 1000); // Operação de saque de 1000 reais
-     *
-     * @param tipo Tipo da operação, podendo ser 'd' ou 's'
-     * @param valor Valor da operação
-     */
-    public Operacao(char tipo, double valor) {
-        this.tipo = tipo;
-        this.valor = valor;
-        data = new Date();
-		
+        System.out.println(valorperacao[0]);
+        System.out.println(valorperacao[1]);
+        System.out.println(valorperacao[2]);
+        System.out.println(valorperacao[3]);
     }
-
 }
+
+//professor como tive dificuldade de fazer da maneira que pediu, fiz um algortimo com o classe operacao
+//mostrando como usa Vetor em Java
